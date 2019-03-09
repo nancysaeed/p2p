8d5b1dac41b011ad9e5482e16413d4f20617e6cc
AyaBadran <ayabadran938@gmail.com>
2019-03-09 14:37:33 +0200

    wait method is added

---
+++ chatApp/src/chatapp/networking/writegui.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapp.networking;
+
+/**
+ *
+ * @author toty
+ */
+public interface writegui {
+    void write(String s);
+}
